f87045d5aa592f765111c89766f28ab6a89d1aa7
Edita Motuze <edita.motuze@gmail.com>
2024-06-10 12:51:11 +0300

    removed unnecessary symbol

--- src/ui/Loader/Loader.jsx
+++ src/ui/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import "./Loader.scss";
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader"></div>;
+      <div className="loader"></div>
     </div>
   );
 };
