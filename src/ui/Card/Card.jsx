3bfd45d4b6a39f421ca87c89eb6f46eceaa1d3eb
Edita Motuze <edita.motuze@gmail.com>
2024-06-10 13:17:13 +0300

    updated navigation from click on cards

--- src/ui/Card/Card.jsx
+++ src/ui/Card/Card.jsx
@@ -1,15 +1,22 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ArrowIcon from "../../assets/desktop/icon-arrow-right.svg";
 import Button from "../Button";
 import "./Card.scss";
 
 const Card = ({ image, heading, to }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigation = () => {
+    if (location.pathname === to) window.location.reload();
+    else navigate(to);
+  };
+
   return (
     <div className="card">
       <img src={image} alt="product" className="card__image" />
       <h6 className="card__heading">{heading}</h6>
-      <Button kind="text" onClick={() => navigate(to)}>
+      <Button kind="text" onClick={() => handleNavigation()}>
         Shop{" "}
         <span>
           <img src={ArrowIcon} alt="arrow right icon" />
